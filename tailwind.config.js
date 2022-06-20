2b7de4478315ef62839609405a9f773f21e9a82c
BrayanValdesG <balexis41@gmail.com>
2022-06-20 17:38:48 -0500

    initial Design

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': { 'max': '375px' },
+      },
+      fontFamily: {
+        'inconsolata': ['Inconsolata', 'monospace'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        "primary": "#FFFFFF",
+        "grayOne": "#333333",
+        "grayTwo": "#4F4F4F",
+        "grayThree": "#BDBDBD",
+      },
+      spacing: {
+        "77": "77px",
+      },
+      fontSize: {
+        '2xlCustom': ['24px', {
+          letterSpacing: '-0.08em',
+          lineHeight: '25px',
+        }],
+        'smCustom': ['14px', {
+          lineHeight: '17px',
+        }]
+      },
+    },
+  },
+  plugins: [],
+}
